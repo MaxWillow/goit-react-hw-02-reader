82887f0403871fb32a56983b517b13753bf8a9ff
Max Verba <maxwillow93@gmail.com>
2019-12-11 00:16:37 +0200

    minor changes

---
+++ build/precache-manifest.45e28e5fb45c6b05801ba9ef438abe9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196a85ee97558ff8332935a5c5876bb6",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "586b489badcbfd9df5ff",
+    "url": "/goit-react-hw-02-reader/static/css/main.d56422ae.chunk.css"
+  },
+  {
+    "revision": "46febe78839327654469",
+    "url": "/goit-react-hw-02-reader/static/js/2.749b6ec6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-reader/static/js/2.749b6ec6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "586b489badcbfd9df5ff",
+    "url": "/goit-react-hw-02-reader/static/js/main.7a1291e2.chunk.js"
+  },
+  {
+    "revision": "c1cc877ab10e9de81056",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.e3843813.js"
+  }
+]);
